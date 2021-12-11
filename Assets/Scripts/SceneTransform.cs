5a9112df91fb542b88c003414cd6940de11436d1
masayushi <4a1e0010@stust.edu.tw>
2021-12-11 18:24:58 +0800

    動畫檔試錯

--- Assets/Scripts/SceneTransform.cs
+++ Assets/Scripts/SceneTransform.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTransform : MonoBehaviour
 {
-	
+    public string GoToScene;
+
+    public void DelayStartGame()
+    {
+        Invoke("StartGame", 0.8f);
+    }
+
+    public void DelayQuitGame()
+    {
+        Invoke("QuitGame", 0.8f);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("GoToScene");
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
 }
